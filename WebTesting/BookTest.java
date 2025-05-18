dc98265b66051dbfe49059bad6bf62feb60b618d
ayush-jaiswal-aj <ayush.jaiswal.7616@gmail.com>
2025-05-18 13:27:04 +0530

    BookTest.java

--- WebTesting/BookTest.java
+++ WebTesting/BookTest.java
@@ -1,51 +1,59 @@
 package Test;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 import java.io.File;
 import java.time.Duration;
 
 public class Test {
+    WebDriver driver;
 
-    public static void main(String[] args) {
-        // Set path to EdgeDriver
+    @BeforeClass
+    public void setup() {
         System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
-
-        WebDriver driver = new EdgeDriver();
+        driver = new EdgeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
 
-        try {
-            // Load Book.html from local system
-            File file = new File("resources/Book.html");
-            String filePath = "file:///" + file.getAbsolutePath().replace("\\", "/");
-            driver.get(filePath); 
+    @Test(priority = 1)
+    public void testFormValidationEmptyFields() {
+        driver.get(getLocalPagePath("resources/Book.html"));
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        Assert.assertTrue(alertText.contains("Please correct the following errors"), "Alert message mismatch!");
+        alert.accept();
+    }
 
-            WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
-            submitButton.click();
-            Thread.sleep(2000);
+    @Test(priority = 2)
+    public void testValidBookingFormSubmission() throws InterruptedException {
+        driver.get(getLocalPagePath("resources/Book.html"));
+        driver.findElement(By.id("name")).sendKeys("Test User");
+        driver.findElement(By.id("phone")).sendKeys("9876543210");
+        driver.findElement(By.id("email")).sendKeys("test@example.com");
+        driver.findElement(By.id("date")).sendKeys("2030-12-31");
+        driver.findElement(By.id("time")).sendKeys("18:00");
+        driver.findElement(By.cssSelector("input[name='persons'][value='2']")).click();
+        driver.findElement(By.id("booking-for")).sendKeys("Birthday");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        Assert.assertTrue(alertText.contains("Booking submitted successfully"), "Success alert mismatch!");
+        alert.accept();
+    }
 
-            driver.findElement(By.id("name")).sendKeys("John Doe");
-            driver.findElement(By.id("phone")).sendKeys("9876543210");
-            driver.findElement(By.id("email")).sendKeys("john@example.com");
-            driver.findElement(By.id("date")).sendKeys("2030-12-31");
-            driver.findElement(By.id("time")).sendKeys("18:30");
-            driver.findElement(By.cssSelector("input[name='persons'][value='2']")).click();
-            driver.findElement(By.id("booking-for")).sendKeys("Birthday");
-
-            submitButton.click();
-            Thread.sleep(2000);
-
-            driver.switchTo().alert().accept();
-
-            System.out.println("Test completed successfully.");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
             driver.quit();
         }
+    }
+
+    private String getLocalPagePath(String relativePath) {
+        File file = new File(relativePath);
+        return "file:///" + file.getAbsolutePath().replace("\\", "/");
     }
 }
