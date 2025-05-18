05c309f98f403346937e4949defd06e7cf15bc40
ayush-jaiswal-aj <ayush.jaiswal.7616@gmail.com>
2025-05-18 18:16:08 +0530

    Update GamePage.java

--- GameTesting/GamePage.java
+++ GameTesting/GamePage.java
@@ -9,15 +9,12 @@ public class GamePage {
     public GamePage(AndroidDriver<MobileElement> driver) {
         this.driver = driver;
     }
-
     public void tapStartButton() {
         driver.findElementByAccessibilityId("startButton").click();
     }
-
     public boolean isScoreVisible() {
         return driver.findElementByAccessibilityId("scoreText").isDisplayed();
     }
-
     public void tapTarget() {
         driver.findElementByXPath("//android.view.View[contains(@content-desc, 'target')]").click();
     }
